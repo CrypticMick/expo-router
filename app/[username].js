2b099e88a54ef14f3feb38cd1d01f817ed28ea6d
Mickey Martinez <mickey@echobind.com>
2023-02-28 08:35:35 -0800

    custom header styling and Home screen title

--- app/[username].js
+++ app/[username].js
@@ -1,6 +1,6 @@
 import { View, Text, Button } from "react-native";
 import React from "react";
-import { useRouter, useSearchParams } from "expo-router";
+import { useRouter, useSearchParams, Stack } from "expo-router";
 
 const profile = () => {
   const router = useRouter();
@@ -14,6 +14,13 @@ const profile = () => {
 
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: "#1E2632" },
+          headerTintColor: "#FFE030",
+        }}
+      />
+
       <Text style={{ fontSize: 30 }}>
         Hello {name} (@{username})
       </Text>
---
+++ app/_layout.js
@@ -0,0 +1,17 @@
+import { Stack } from "expo-router";
+
+export default () => {
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#FFE030",
+        },
+        headerTintColor: "#1E2632",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    />
+  );
+};
