8d58cc9f97cb78b0bc97842b7cf37c150e2561fe
Mickey Martinez <mickey@echobind.com>
2023-03-03 09:36:16 -0800

    Nested messages Navigator with Stack layout

--- app/home/messages/index.js
+++ app/home/messages/index.js
@@ -1,10 +1,13 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { Link } from "expo-router";
 
 const Messages = () => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Messages</Text>
+      <Link href={"home/messages/messageRoom"}>Messages with Snoop Dogg</Link>
+
+      <Link href={"home/messages/messageRoom"}>Messages with Nate Dogg</Link>
     </View>
   );
 };
---
+++ app/home/messages/messageRoom.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const messageRoom = () => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>messageRoom</Text>
+    </View>
+  );
+};
+
+export default messageRoom;
